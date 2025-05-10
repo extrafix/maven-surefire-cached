c35f799ad32bec044d44d9e568c53dedb9045cc2
seregamorph <serega.morph@gmail.com>
2025-05-10 12:01:38 +0200

    Validate report root tag

--- surefire-cached-extension/src/main/java/com/github/seregamorph/maven/test/core/TestSuiteReport.java
+++ surefire-cached-extension/src/main/java/com/github/seregamorph/maven/test/core/TestSuiteReport.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
 /**
@@ -19,13 +20,19 @@ public record TestSuiteReport(String name, BigDecimal time, int tests, int error
             var factory = DocumentBuilderFactory.newInstance();
             var docBuilder = factory.newDocumentBuilder();
             var root = docBuilder.parse(file);
+            Element rootElement = root.getDocumentElement();
+            String tagName = rootElement.getTagName();
+            if (!"testsuite".equals(tagName)) {
+                throw new IllegalArgumentException("Not a test suite report file: "
+                    + file + " with root element '" + tagName + "'");
+            }
 
             // test class name
-            var name = root.getDocumentElement().getAttribute("name");
-            var time = new BigDecimal(root.getDocumentElement().getAttribute("time"));
-            var tests = Integer.parseInt(root.getDocumentElement().getAttribute("tests"));
-            var errors = Integer.parseInt(root.getDocumentElement().getAttribute("errors"));
-            var failures = Integer.parseInt(root.getDocumentElement().getAttribute("failures"));
+            var name = rootElement.getAttribute("name");
+            var time = new BigDecimal(rootElement.getAttribute("time"));
+            var tests = Integer.parseInt(rootElement.getAttribute("tests"));
+            var errors = Integer.parseInt(rootElement.getAttribute("errors"));
+            var failures = Integer.parseInt(rootElement.getAttribute("failures"));
 
             return new TestSuiteReport(name, time, tests, errors, failures);
         } catch (ParserConfigurationException | SAXException | IOException e) {
