d3e5a2edbce6bea2cd6f37ced23314f8d069140d
seregamorph <serega.morph@gmail.com>
2025-05-14 17:58:56 +0200

    Author

--- surefire-cached-extension/src/main/java/com/github/seregamorph/maven/test/util/MavenPropertyUtils.java
+++ surefire-cached-extension/src/main/java/com/github/seregamorph/maven/test/util/MavenPropertyUtils.java
@@ -4,6 +4,9 @@ import javax.annotation.Nullable;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.project.MavenProject;
 
+/**
+ * @author Sergey Chernov
+ */
 public final class MavenPropertyUtils {
 
     @Nullable
