96e2cce383f939b65a396eb4b613ab1c83b9419f
seregamorph <serega.morph@gmail.com>
2025-05-15 23:22:53 +0200

    SoftReferenceMemoryStorage

--- surefire-cached-common/src/main/java/com/github/seregamorph/maven/test/storage/SoftReferenceMemoryStorage.java
+++ surefire-cached-common/src/main/java/com/github/seregamorph/maven/test/storage/SoftReferenceMemoryStorage.java
@@ -1,22 +1,23 @@
 package com.github.seregamorph.maven.test.storage;
 
 import com.github.seregamorph.maven.test.common.CacheEntryKey;
+import java.lang.ref.SoftReference;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import javax.annotation.Nullable;
 
 /**
- * In-memory cache LRU storage.
+ * In-memory cache LRU storage with SoftReference.
  *
  * @author Sergey Chernov
  */
-public class MemoryStorage implements CacheStorage {
+public class SoftReferenceMemoryStorage implements CacheStorage {
 
-    private final Map<CacheEntryKey, Map<String, byte[]>> cache = new LinkedHashMap<>();
+    private final Map<CacheEntryKey, SoftReference<Map<String, byte[]>>> cache = new LinkedHashMap<>();
 
     private final int size;
 
-    public MemoryStorage(int size) {
+    public SoftReferenceMemoryStorage(int size) {
         if (size <= 0) {
             throw new IllegalArgumentException("size must be > 0");
         }
@@ -27,12 +28,16 @@ public class MemoryStorage implements CacheStorage {
     @Nullable
     public byte[] read(CacheEntryKey cacheEntryKey, String fileName) {
         synchronized (cache) {
-            var cacheEntry = cache.remove(cacheEntryKey);
+            var softReferenceCacheEntry = cache.remove(cacheEntryKey);
+            if (softReferenceCacheEntry == null) {
+                return null;
+            }
+            var cacheEntry = softReferenceCacheEntry.get();
             if (cacheEntry == null) {
                 return null;
             }
             // LRU
-            cache.put(cacheEntryKey, cacheEntry);
+            cache.put(cacheEntryKey, softReferenceCacheEntry);
             return cacheEntry.get(fileName);
         }
     }
@@ -41,20 +46,22 @@ public class MemoryStorage implements CacheStorage {
     public int write(CacheEntryKey cacheEntryKey, String fileName, byte[] value) {
         int deleted = 0;
         synchronized (cache) {
-            var cacheEntry = cache.remove(cacheEntryKey);
+            var softReferenceCacheEntry = cache.remove(cacheEntryKey);
+            var cacheEntry = softReferenceCacheEntry == null ? null : softReferenceCacheEntry.get();
             if (cacheEntry == null) {
                 if (cache.size() >= size) {
                     // LRU
                     var iterator = cache.entrySet().iterator();
-                    var deletedEntry = iterator.next().getValue();
-                    synchronized (deletedEntry) {
-                        deleted = deletedEntry.size();
+                    var deletedReference = iterator.next().getValue();
+                    synchronized (deletedReference) {
+                        var deletedEntry = deletedReference.get();
+                        deleted = deletedEntry == null ? 0 : deletedEntry.size();
                     }
                     iterator.remove();
                 }
                 cacheEntry = new LinkedHashMap<>();
             }
-            cache.put(cacheEntryKey, cacheEntry);
+            cache.put(cacheEntryKey, new SoftReference<>(cacheEntry));
             cacheEntry.put(fileName, value);
         }
         return deleted;
