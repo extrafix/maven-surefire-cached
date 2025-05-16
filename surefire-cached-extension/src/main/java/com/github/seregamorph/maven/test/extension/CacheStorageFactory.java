3f3d662b987ce4acd7736f1dc557656029c7d3bf
seregamorph <serega.morph@gmail.com>
2025-05-16 18:08:35 +0200

    FailsafeCacheStorage for HTTP

--- surefire-cached-extension/src/main/java/com/github/seregamorph/maven/test/extension/CacheStorageFactory.java
+++ surefire-cached-extension/src/main/java/com/github/seregamorph/maven/test/extension/CacheStorageFactory.java
@@ -1,6 +1,7 @@
 package com.github.seregamorph.maven.test.extension;
 
 import com.github.seregamorph.maven.test.storage.CacheStorage;
+import com.github.seregamorph.maven.test.storage.FailsafeCacheStorage;
 import com.github.seregamorph.maven.test.storage.FileCacheStorage;
 import com.github.seregamorph.maven.test.storage.HttpCacheStorage;
 import java.io.File;
@@ -11,7 +12,8 @@ public class CacheStorageFactory {
     public static CacheStorage createCacheStorage(String cacheStorageUrl) {
         //noinspection HttpUrlsUsage
         if (cacheStorageUrl.startsWith("http://") || cacheStorageUrl.startsWith("https://")) {
-            return new HttpCacheStorage(URI.create(cacheStorageUrl));
+            // todo configuration
+            return new FailsafeCacheStorage(new HttpCacheStorage(URI.create(cacheStorageUrl)), 4);
         }
 
         return new FileCacheStorage(new File(cacheStorageUrl));
